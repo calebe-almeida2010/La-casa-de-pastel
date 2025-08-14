d4a64c2d9e329822d4f65313650a6fa4436cf000
calebe-almeida2010 <calebe.almeida@escola.pr.gov.br>
2025-08-14 12:10:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Vetores de objetos
+const sabores = [
+    { id: "pastel_de_carne", nome: "Pastel_de_Carne", preco: 3,99 :},
+    { id: "paste_de_queijo", nome: "Pastel_de_Queijo", preco: 3,99 :},
+    { id: "pastel_de_frango", nome: "Pastel_de_Frango", preco: 3,99 :},
+    { id: "pastel_de_pizza", nome: "Pastel_de_Pizza", preco: 3,99 :},
+    { id: "pastel_de_carne_com_ovo", nome: "Paste_de_Carne_com_Ovo", preco: 3,99 :}
+];
